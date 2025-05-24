15c0f71277725d8f9a6ad6d0900263dcce1d237a
DRGULEN <dr.huseyingulen@hotmail.com>
2025-05-24 17:53:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
+Python 3.13.3 (tags/v3.13.3, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license" for more information.
 >>> mport requests
   File "<python-input-0>", line 1
